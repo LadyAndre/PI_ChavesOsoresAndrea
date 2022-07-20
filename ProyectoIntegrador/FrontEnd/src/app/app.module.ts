c6264fd066daee52acea3a70036c6cc3908d33db
LadyAndre <andychavs20@gmail.com>
2022-07-20 19:02:54 -0300

    .

--- ProyectoIntegrador/FrontEnd/src/app/app.module.ts
+++ ProyectoIntegrador/FrontEnd/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -13,7 +12,10 @@ import { HardysComponent } from './components/hardys/hardys.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HttpClientModule} from '@angular/common/http'
+import { HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component'
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,10 +28,13 @@ import { HttpClientModule} from '@angular/common/http'
     HardysComponent,
     ProyectoComponent,
     FooterComponent,
+    HomeComponent,
+    LoginComponent,
   ],
   imports: [
      BrowserModule,
     AppRoutingModule,
+    FormsModule,
      NgCircleProgressModule.forRoot({}),
      HttpClientModule
   ],
